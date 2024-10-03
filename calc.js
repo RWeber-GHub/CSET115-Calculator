6607591c2d8eade19b2e413f8c8b3ad632e52079
RWeber <rtw3646@gmail.com>
2024-10-03 10:20:58 -0400

    added display and clear function

---
+++ calc.js
@@ -0,0 +1,15 @@
+let num1 = 0
+let num2 = 0
+let op = ""
+
+
+function display(val){
+    document.getElementById("result").value += val
+}
+function clr(){
+    document.getElementById("result").value = ""
+}
+function clrall(){
+    document.getElementById("result").value = ""
+    document.getElementById("memory").value = ""
+}
